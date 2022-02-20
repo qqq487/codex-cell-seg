a7675b46ead08d445790b28e18c4e92fc33ff994
qqq487 <chacotw487@gmail.com>
2022-02-20 15:46:32 +0800

    rm useless code

--- .ipynb_checkpoints/infer-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-# python predict-single.py --model 'checkpoints_single_1000/checkpoint_epoch901.pth' --input_folder 'test_imgs_single_nuclei/' --mask-threshold 0.5 --scale 1
-python predict-multi.py --model 'checkpoints_multi-5-3img_refined_15000_wo23_lastCAlastSA/checkpoint_epoch4001.pth' --input_folder 'test_imgs_multi/' --mask-threshold 0.5 --scale 1
